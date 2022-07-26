39b622f65255d63e0223566b1d60ffac2df3df73
Bhoomika Jaiman <bjaiman2001@gmail.com>
2022-07-26 19:59:55 +0530

    demo6

---
+++ test-case/cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+// test suite name
+describe('Tutorialspoint Test', function () {
+  // Test case
+     it('Scenario 1', function (){
+        // test step for URL launching
+        cy.visit("http://localhost:8080/TomcatMavenApp/");
+     });
+  });
