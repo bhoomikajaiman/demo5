da09b4450558c8fcf068fa653ac5db8a33031c92
Bhoomika Jaiman <bjaiman2001@gmail.com>
2022-07-27 18:12:11 +0530

    okayyy

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('My First Cypress Test', function() {
+  it('Visits the login page', function() {
+  //Visit the Demo QA Website
+  cy.wait(500)
+  cy.visit("http://localhost:8080/TomcatMavenApp/");
+})
 })
