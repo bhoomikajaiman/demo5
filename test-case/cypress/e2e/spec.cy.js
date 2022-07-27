d17f9d3cc9c04281f9ad98f695b00b98207c9451
Bhoomika Jaiman <bjaiman2001@gmail.com>
2022-07-27 18:00:49 +0530

    okay

--- test-case/cypress/e2e/spec.cy.js
+++ test-case/cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('My First Cypress Test', function() {
   it('Visits the login page', function() {
   //Visit the Demo QA Website
+  cy.wait(500)
   cy.visit("http://localhost:8080/TomcatMavenApp/");
 })
 })
